9b4a21c670cbb0ad4dccc322d7be54b711e6fa2c
Vaibhavi Rohilla <62430272+vaibhavirohilla741@users.noreply.github.com>
2021-04-04 17:31:12 +0530

    Add files via upload

--- bloodDonationScript.js
+++ bloodDonationScript.js
@@ -1,8 +1,19 @@
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function viewPassword()
+{
+  var passwordInput = document.getElementById('password-field');
+  var passStatus = document.getElementById('pass-status');
+ 
+  if (passwordInput.type == 'password'){
+    passwordInput.type='text';
+    passStatus.className='fa fa-eye-slash';
+    
+  }
+  else{
+    passwordInput.type='password';
+    passStatus.className='fa fa-eye';
   }
 }
+
+
+
+Resources
